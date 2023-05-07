7f10e6d38e023f2277d5efd543e24e5d0520a87d
SebaSinhue <sebasinhue1@gmail.com>
2023-05-06 23:38:54 -0600

    Se agrega el Proyecto

---
+++ src/main/java/org/example/MainVentanaSwing.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class MainVentanaSwing {
+    public static void main(String[] args) {
+        VentanaSwing ventana = new VentanaSwing("Centigrados a Farenheit");
+    }
+}
